d590289cb349ac3ec738d32b3a1d600351f16b42
s25670-pj <92204700+s25670-pj@users.noreply.github.com>
2023-01-21 16:58:44 +0100

    add tests

---
+++ s25670Bank/src/test/java/pl/pjwstk/s25670Bank/TransferServiceMockTest.java
@@ -0,0 +1,23 @@
+package pl.pjwstk.s25670Bank;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class TransferServiceMockTest {
+    @Mock
+    private ClientStorage clientStorage;
+    @InjectMocks
+    private TransferService transferService;
+
+}
